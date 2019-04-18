9e44e59d1dfacc2731163b4af4a930e0fea03f88
ajayruhela <jrhl0013@gmail.com>
2019-04-18 17:46:32 +0530

    8 files modified, 2 added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { first } from 'rxjs/operators';
 
-import { User } from '@/_models';
-import { UserService, AuthenticationService } from '@/_services';
+import { User } from '../_models';
+import { UserService, AuthenticationService } from '../_services';
 
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent {
